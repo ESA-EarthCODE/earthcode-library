ac02a89698beab37745ee29fffb2de311cf5c3b4
sunnydean <dean.samarjiev@gmail.com>
2026-04-01 16:43:34 +0100

    adding end-to-end integration test for generation, integration, validation - and making corrections to yaml templates

--- earthcode/generators/product_generator.py
+++ earthcode/generators/product_generator.py
@@ -10,50 +10,61 @@ from earthcode.static import create_product_collection, ProductCollectionMetadat
 from earthcode.git_add import save_product_collection_to_catalog
 
 
-logging.basicConfig(stream=sys.stdout, encoding='utf-8', level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, encoding="utf-8", level=logging.INFO)
 log = logging.getLogger()
 
 
 def create_product_stac_from_template(project_yaml, osc_path):
-    with (open(project_yaml, 'r') as file):
+    with open(project_yaml, "r") as file:
         data = yaml.safe_load(file)
 
         for k, v in data.items():
             if v is None:
-                log.error(f"The Project YAML contains an empty value for the following field: {k}")
-                raise Exception(f"The Project YAML contains an empty value for the following field: {k}")
+                log.error(
+                    f"The Project YAML contains an empty value for the following field: {k}"
+                )
+                raise Exception(
+                    f"The Project YAML contains an empty value for the following field: {k}"
+                )
 
         # Define spatial and temporal extent
-        spatial_extent = pystac.SpatialExtent(data['extent']['spatial']['bbox']).bboxes
-        temporal_start = datetime.strptime(data['extent']['temporal']['start'], "%Y-%m-%dT%H:%M:%SZ")
-        temporal_end = datetime.strptime(data['extent']['temporal']['end'], "%Y-%m-%dT%H:%M:%SZ")
+        spatial_extent = pystac.SpatialExtent(data["extent"]["spatial"]["bbox"]).bboxes
+        temporal_start = datetime.strptime(
+            data["extent"]["temporal"]["start"], "%Y-%m-%dT%H:%M:%SZ"
+        )
+        temporal_end = datetime.strptime(
+            data["extent"]["temporal"]["end"], "%Y-%m-%dT%H:%M:%SZ"
+        )
 
         # optional
-        product_license = data['license']
-        if product_license == 'other':
-            product_license_link = data['license_link']
+        product_license = data["license"]
+        if product_license == "other":
+            product_license_link = data["license_link"]
         else:
             product_license_link = None
 
+        via_link = data.get("via_link", None)
+
     product_metadata = ProductCollectionMetadata(
-        product_id=data['id'],
-        product_title=data['title'],
-        product_description=data['description'],
-        product_keywords=data['keywords'],
-        product_status=data['status'],
-        product_region=data['region'],
-        product_themes=data['themes'],
-        product_missions=data['missions'],
-        product_variables=data['variables'],
-        project_id=data['project'],
-        project_title=data['project-title'],
-        product_parameters=data['cf_parameters'],
-        product_doi=data['sci:doi'],
+        product_id=data["id"],
+        product_title=data["title"],
+        product_description=data["description"],
+        product_keywords=data["keywords"],
+        product_status=data["status"],
+        product_region=data["region"],
+        product_themes=data["themes"],
+        product_missions=data["missions"],
+        product_variables=data["variables"],
+        project_id=data["project"],
+        project_title=data["project-title"],
+        product_parameters=data["cf_parameters"],
+        product_doi=data["sci:doi"],
         product_bbox=spatial_extent,
         product_start_datetime=temporal_start,
         product_end_datetime=temporal_end,
         product_license=product_license,
         license_link=product_license_link,
+        access_link=via_link,
     )
 
     product_collection = create_product_collection(product_metadata)
